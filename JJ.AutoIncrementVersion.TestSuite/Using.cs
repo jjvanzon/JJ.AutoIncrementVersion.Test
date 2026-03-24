eca914e99cbee1d31ae4963b0663fdffe25da4c1
jjvanzon <jjvanzon79@gmail.com>
2026-03-24 21:50:58 +0100

    🥼 Automate Manual Tests - Re-admin test proj props - Use newer(ish) test framework and remove unnecessary proj props.

--- JJ.AutoIncrementVersion.TestSuite/Using.cs
+++ JJ.AutoIncrementVersion.TestSuite/Using.cs
@@ -2,6 +2,7 @@
 global using System.Text;
 global using System.Text.RegularExpressions;
 global using System.Xml.Linq;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using JJ.AutoIncrementVersion.TestSuite.Helpers;
 global using static System.StringComparison;
 global using static System.Text.RegularExpressions.RegexOptions;
